41732e1b39142861bf5a0c8ae4a0d611db77af68
KyleLEI <leizhao2@gmail.com>
2019-04-23 15:54:45 +0800

    updated jacobian A, working on U

--- include/ekf/EKF.h
+++ include/ekf/EKF.h
@@ -2,8 +2,9 @@
 
 class EKF{
 public:
-    EKF(const Eigen::VectorXd initial_state);
-    Eigen::VectorXd predict(Eigen::VectorXd u_t);
+    EKF(const Eigen::VectorXd initial_mean, const Eigen::MatrixXd initial_cov,
+        const Eigen::MatrixXd Q_in, const Eigen::MatrixXd R_in);
+    Eigen::VectorXd predict(const Eigen::VectorXd u_t, const double dt); 
     void update();
 
     //Eigen::MatrixXd getCovarianceMatrix() const{return sigma;}
@@ -11,25 +12,31 @@ public:
 
 private:
     /* Private Function */
-    Eigen::VectorXd f();
-    Eigen::VectorXd A();
-    //Eigen::VectorXd B();
-    Eigen::VectorXd U();
+    Eigen::VectorXd f0();    // returns f(mu_(t-1), u_t, 0)
+    Eigen::MatrixXd A();
+    Eigen::VectorXd F(const double dt);
+    Eigen::MatrixXd U();
+    Eigen::MatrixXd V(const double dt);
     Eigen::MatrixXd C();
     Eigen::MatrixXd W();
     Eigen::MatrixXd K();
 
     /* Private Data */
-    Eigen::MatrixXd sigma; // covariance matrix, (5*3+2*3), PNP
-    Eigen::VectorXd mu; // mean, (5*3+2*3), PNP
+    Eigen::MatrixXd sigma;  // covariance matrix
+    Eigen::VectorXd mu;     // mean
+    Eigen::MatrixXd sigma_hat;
+    Eigen::VectorXd mu_hat;
+    Eigen::MatrixXd Q;
+    Eigen::MatrixXd R;
 
-    /* State X */
+    /* Mean of State, mu */
     double x,y,z;           // X1: position p,      PNP
     double roll,pitch,yaw;  // X2: orientation q,   PNP
     double vx,vy,vz;        // X3: linear vel p_dot,OPTFLOW
     double bgx,bgy,bgz;     // X4: gyro bias b_g
     double bax,bay,baz;     // X5: acc bias b_a
-
+    void updateMean();      // Always call this after updating mu
+    
     /* Noise n */
     double ngx,ngy,ngz;     // gyro noise
     double nax,nay,naz;     // acc noise
--- src/EKF.cpp
+++ src/EKF.cpp
@@ -3,42 +3,149 @@
 
 using namespace Eigen;
 
-EKF::EKF(const VectorXd is){
-    sigma = MatrixXd::Zero(21,21);
-    mu = VectorXd::Zero(21);
-    x = is[0];
-    y = is[1];
-    z = is[2];
-    roll = is[3];
-    pitch = is[4];
-    yaw = is[5];
-    vx = is[6];
-    vy = is[7];
-    vz = is[8];
-    bgx = is[9];
-    bgy = is[10];
-    bgz = is[11];
-    bax = is[12];
-    bay = is[13];
-    baz = is[14];
-
-    mu.topRows(15) = is;
-    sigma.diagonal()<<1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1;
+EKF::EKF(const VectorXd is, const MatrixXd ic, const MatrixXd Q_in, const MatrixXd R_in):sigma_hat(15,15),mu_hat(15),sigma(15,15),mu(15){
+    mu = is;
+    updateMean();
+    sigma = ic;
+    Q = Q_in;
+    R = R_in;
 }
 
-VectorXd EKF::A(){
-    /* Linearized about x = mu_(t-1), u = u_t, Q = 0 */
+inline void EKF::updateMean(){
+    x = mu[0];
+    y = mu[1];
+    z = mu[2];
+    roll = mu[3];
+    pitch = mu[4];
+    yaw = mu[5];
+    vx = mu[6];
+    vy = mu[7];
+    vz = mu[8];
+    bgx = mu[9];
+    bgy = mu[10];
+    bgz = mu[11];
+    bax = mu[12];
+    bay = mu[13];
+    baz = mu[14];
+}
+
+inline VectorXd EKF::A(){
     /* Everything haven't been updated yet, so x = mu_(t-1) */
     /*** Assuming u has been updated before calling this func ***/
     VectorXd A = VectorXd::Zero(15);
-    A[4] = - (cos(pitch)*sin(roll)*(bgx + ngx - wx))/cos(roll) - (sin(pitch)*sin(roll)*(bgz + ngz - wz))/cos(roll);
+    A[4] = - (cos(pitch)*sin(roll)*(bgx - wx))/cos(roll) - (sin(pitch)*sin(roll)*(bgz - wz))/cos(roll);
     return A;
 }
 
-VectorXd EKF::U(){
-    VectorXd U(15);
-    U<<0,0,0,-cos(pitch),-1,-cos(pitch)/cos(roll),sin(pitch)*sin(roll)*sin(yaw) - cos(pitch)*cos(yaw),-cos(roll)*cos(yaw),-cos(pitch)*cos(roll),1,1,1,1,1,1;
+inline MatrixXd EKF::A(){
+    /*** Assuming u has been updated before the call ***/
+    MatrixXd A = MatrixXd::Zero(15,15);
+
+    /* df0/droll */
+    A(4,3) = (bgz*cos(pitch) - bgx*sin(pitch) - wz*cos(pitch) + wx*sin(pitch))/cos(roll)*cos(roll);
+    A(5,3) = (sin(pitch)*sin(roll)*(bgx - wx))/(cos(roll)*cos(roll)) - (cos(pitch)*sin(roll)*(bgz - wz))/(cos(roll)*cos(roll));
+    A(6,3) = sin(roll)*sin(yaw)*(ay - bay) + cos(pitch)*cos(roll)*sin(yaw)*(az - baz) - cos(roll)*sin(pitch)*sin(yaw)*(ax - bax);
+    A(7,3) = cos(roll)*cos(yaw)*sin(pitch)*(ax - bax) - cos(pitch)*cos(roll)*cos(yaw)*(az - baz) - cos(yaw)*sin(roll)*(ay - bay);
+    A(8,3) = cos(roll)*(ay - bay) - cos(pitch)*sin(roll)*(az - baz) + sin(pitch)*sin(roll)*(ax - bax);
+
+    /* df0/dpitch */
+    A(3,4) = sin(pitch)*(bgx - wx) - cos(pitch)*(bgz - wz);
+    A(4,4) = - (cos(pitch)*sin(roll)*(bgx - wx))/cos(roll) - (sin(pitch)*sin(roll)*(bgz - wz))/cos(roll);
+    A(5,4) = (cos(pitch)*(bgx - wx))/cos(roll) + (sin(pitch)*(bgz - wz))/cos(roll);
+    A(6,4) = (az - baz)*(cos(pitch)*cos(yaw) - sin(pitch)*sin(roll)*sin(yaw)) - (ax - bax)*(cos(yaw)*sin(pitch) + cos(pitch)*sin(roll)*sin(yaw));
+    A(7,4) = (az - baz)*(cos(pitch)*sin(yaw) + cos(yaw)*sin(pitch)*sin(roll)) - (ax - bax)*(sin(pitch)*sin(yaw) - cos(pitch)*cos(yaw)*sin(roll));
+    A(8,4) = - cos(pitch)*cos(roll)*(ax - bax) - cos(roll)*sin(pitch)*(az - baz);
+
+    /* df0/dyaw */
+    A(6,5) = - (ax - bax)*(cos(pitch)*sin(yaw) + cos(yaw)*sin(pitch)*sin(roll)) - (az - baz)*(sin(pitch)*sin(yaw) - cos(pitch)*cos(yaw)*sin(roll)) - cos(roll)*cos(yaw)*(ay - bay);
+    A(7,5) = (ax - bax)*(cos(pitch)*cos(yaw) - sin(pitch)*sin(roll)*sin(yaw)) + (az - baz)*(cos(yaw)*sin(pitch) + cos(pitch)*sin(roll)*sin(yaw)) - cos(roll)*sin(yaw)*(ay - bay);
+
+    /* df0/dp_dot */
+    A(0,6) = 1.0;
+    A(1,7) = 1.0;
+    A(2,8) = 1.0;
+
+    /* df0/dbgx */
+    A(3,9) = -cos(pitch);
+    A(4,9) = -(sin(pitch)*sin(roll))/cos(roll);
+    A(5,9) = sin(pitch)/cos(roll);
+
+    /* df0/dbgy */
+    A(4,10) = -1;
+
+    /* df0/dbgz */
+    A(3,11) = -sin(pitch);
+    A(4,11) = (cos(pitch)*sin(roll))/cos(roll);
+    A(5,11) = -cos(pitch)/cos(roll);
+
+    /* df0/dbax */
+    A(6,12) = sin(pitch)*sin(roll)*sin(yaw) - cos(pitch)*cos(yaw);
+    A(7,12) = - cos(pitch)*sin(yaw) - cos(yaw)*sin(pitch)*sin(roll);
+    A(8,12) = cos(roll)*sin(pitch);
+
+    /* df0/dbay */
+    A(6,13) = cos(roll)*sin(yaw);
+    A(7,13) = -cos(roll)*cos(yaw);
+    A(8,13) = -sin(roll);
+
+    /* df0/dbaz */
+    A(6,14) = - cos(yaw)*sin(pitch) - cos(pitch)*sin(roll)*sin(yaw);
+    A(7,14) = cos(pitch)*cos(yaw)*sin(roll) - sin(pitch)*sin(yaw);
+    A(8,14) = -cos(pitch)*cos(roll);
+
+    return A;
+}
+
+inline MatrixXd EKF::F(const double dt){
+    MatrixXd I = MatrixXd::Identity(15,15);
+    return I+dt*A();
+}
+
+inline MatrixXd EKF::U(){
+    /*** Assuming u has been updated before calling ***/
+    MatrixXd U = MatrixXd::Zero(15,15);
+
+    /* df/dngx */
+    U(3,3) = -cos(pitch);
+    U(4,3) = -(sin(pitch)*sin(roll))/cos(roll);
+    U(5,3) = sin(pitch)/cos(roll);
+
+    /* df/dngy */
+
+
     return U;
 }
 
+inline VectorXd EKF::V(const double dt){
+    return dt*U();
+}
 
+VectorXd EKF::f0(){
+    VectorXd f(15);
+    f<<vx,vy,vz,
+        - cos(pitch)*(bgx - wx) - sin(pitch)*(bgz - wz),
+        wy - bgy + (cos(pitch)*sin(roll)*(bgz - wz))/cos(roll) - (sin(pitch)*sin(roll)*(bgx - wx))/cos(roll),
+        (sin(pitch)*(bgx - wx))/cos(roll) - (cos(pitch)*(bgz - wz))/cos(roll),
+        (ax - bax)*(cos(pitch)*cos(yaw) - sin(pitch)*sin(roll)*sin(yaw)) + (az - baz)*(cos(yaw)*sin(pitch) + cos(pitch)*sin(roll)*sin(yaw)) - cos(roll)*sin(yaw)*(ay - bay),
+        (ax - bax)*(cos(pitch)*sin(yaw) + cos(yaw)*sin(pitch)*sin(roll)) + (az - baz)*(sin(pitch)*sin(yaw) - cos(pitch)*cos(yaw)*sin(roll)) + cos(roll)*cos(yaw)*(ay - bay),
+        sin(roll)*(ay - bay) + cos(pitch)*cos(roll)*(az - baz) - cos(roll)*sin(pitch)*(ax - bax) - 9.81,
+        0,0,0,0,0,0;
+    return f;
+}
+
+VectorXd EKF::predict(const VectorXd u, const double dt){
+    /* Update u */
+    wx = u[0];
+    wy = u[1];
+    wz = u[2];
+    ax = u[3];
+    ay = u[4];
+    az = u[5];
+
+    /* Estimate mean */
+    mu_hat = mu + dt*f0(); // mu here is mu_(t-1)
+
+    /* Estimate coveriance */
+    sigma_hat = F(dt)*sigma*(F(dt))
+    return mu_hat;
+}
