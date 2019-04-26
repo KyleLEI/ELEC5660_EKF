644f2c3ec8711983cab8ac6f5cf66a1b3bd5fd72
KyleLEI <leizhao2@gmail.com>
2019-04-26 14:32:34 +0800

    optimize Rci

--- src/ekf_node.cpp
+++ src/ekf_node.cpp
@@ -18,6 +18,24 @@ MatrixXd R_optflow = MatrixXd::Identity(6,6);
 MatrixXd R_optflowl = MatrixXd::Identity(3,3);
 double imu_time, last_imu_time=-1.0; 
 
+void publish_odom(VectorXd m){
+    double x_m=m(0),y_m=m(1),z_m=m(2),roll_m=m(3),pitch_m=m(4),yaw_m=m(5);
+    Vector3d rpy_m{roll_m,pitch_m,yaw_m};
+    Matrix3d R_m = EKF::util_RPYToRot(rpy_m);
+    Quaterniond Q_ekf(R_m);
+
+    nav_msgs::Odometry odom_ekf;
+    odom_ekf.header.frame_id = "world";
+    odom_ekf.pose.pose.position.x = x_m;
+    odom_ekf.pose.pose.position.y = y_m;
+    odom_ekf.pose.pose.position.z = z_m;
+    odom_ekf.pose.pose.orientation.w = Q_ekf.w();
+    odom_ekf.pose.pose.orientation.x = Q_ekf.x();
+    odom_ekf.pose.pose.orientation.y = Q_ekf.y();
+    odom_ekf.pose.pose.orientation.z = Q_ekf.z();
+    odom_pub.publish(odom_ekf);
+}
+
 void imu_callback(const sensor_msgs::Imu::ConstPtr &msg)
 {
     if(ekf==nullptr)
@@ -38,29 +56,14 @@ void imu_callback(const sensor_msgs::Imu::ConstPtr &msg)
     //cout<<"Predict =\n"<<ekf->predict(u,imu_time-last_imu_time)<<endl;
     ekf->predict(u,imu_time-last_imu_time);
     last_imu_time = imu_time;
-}
-
-void publish_odom(VectorXd m){
-    double x_m=m(0),y_m=m(1),z_m=m(2),roll_m=m(3),pitch_m=m(4),yaw_m=m(5);
-    Vector3d rpy_m{roll_m,pitch_m,yaw_m};
-    Matrix3d R_m = EKF::util_RPYToRot(rpy_m);
-    Quaterniond Q_ekf(R_m);
-
-    nav_msgs::Odometry odom_ekf;
-    odom_ekf.header.frame_id = "world";
-    odom_ekf.pose.pose.position.x = x_m;
-    odom_ekf.pose.pose.position.y = y_m;
-    odom_ekf.pose.pose.position.z = z_m;
-    odom_ekf.pose.pose.orientation.w = Q_ekf.w();
-    odom_ekf.pose.pose.orientation.x = Q_ekf.x();
-    odom_ekf.pose.pose.orientation.y = Q_ekf.y();
-    odom_ekf.pose.pose.orientation.z = Q_ekf.z();
-    odom_pub.publish(odom_ekf);
+    //publish_odom(ekf->getMean());
 }
 
 //Rotation from the camera frame to the IMU frame
 Vector3d T_ic;
-Eigen::Matrix3d R_ic;
+Matrix3d R_ic;
+Vector3d T_ci;
+Matrix3d R_ci;
 Matrix3d R_wm;
 Vector3d T_wm;
 void pnp_callback(const nav_msgs::Odometry::ConstPtr &msg)
@@ -94,11 +97,8 @@ void pnp_callback(const nav_msgs::Odometry::ConstPtr &msg)
         msg->pose.pose.orientation.z
     ).toRotationMatrix();
 
-    Vector3d T_mc = -1*T_cm;
-    Matrix3d R_mc = R_cm.transpose();
-    Vector3d T_ci = -1*T_ic;
-    Matrix3d R_ci = R_ic.transpose();
-   
+    Matrix3d R_mc = R_cm.transpose(); 
+    Vector3d T_mc = -1*R_mc*T_cm;
     Matrix3d R_wi = R_wm*R_mc*R_ci;
     Vector3d T_wi = R_wm*(R_mc*T_ci+T_mc)+T_wm;
         
@@ -140,20 +140,20 @@ void optflow_callback(const nav_msgs::Odometry::ConstPtr &msg){
     z<<p_i,p_dot_i;
     //z(0)=0;z(1)=0;z(5)=0;
     cout<<"z2 = \n"<<z<<endl;
-    ekf->update2(z);
+    //ekf->update2(z);
     //std::cout<<"sigma2 =\n"<<ekf->getCovariance().diagonal()<<std::endl;
     //std::cout<<"mu2 =\n"<<ekf->getMean()<<std::endl<<std::endl;
     publish_odom(ekf->getMean());
 }
 
 void optflow_callback_linear(const nav_msgs::Odometry::ConstPtr &msg){
-    double  of_vx = msg->twist.twist.linear.y,
-            of_vy = msg->twist.twist.linear.x,
+    double  of_vx = -msg->twist.twist.linear.y,
+            of_vy = -msg->twist.twist.linear.x,
             of_z = msg->pose.pose.position.z;
     Vector3d z{of_vx,of_vy,of_z};
     ekf->update2l(z);
-    //std::cout<<"sigma =\n"<<ekf->getCovariance().diagonal()<<std::endl;
-    std::cout<<"mu =\n"<<ekf->getMean()<<std::endl<<std::endl;
+    std::cout<<"sigma2 =\n"<<ekf->getCovariance().diagonal()<<std::endl;
+    //std::cout<<"mu2 =\n"<<ekf->getMean()<<std::endl<<std::endl;
     publish_odom(ekf->getMean());
 }
 
@@ -169,6 +169,8 @@ int main(int argc, char **argv)
             -1,0,0,
             0,0,-1;
     T_ic<<-0.1, 0, -0.03;
+    R_ci = R_ic.transpose();
+    T_ci = -1*R_ci*T_ic;
     R_wm<<  0,1,0,
             1,0,0,
             0,0,-1;
